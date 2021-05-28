b5a28d1f24857dd1dd1a34cd15937b2b5fad821e
Javier19-cmd <63107636+Javier19-cmd@users.noreply.github.com>
2021-05-28 14:30:05 -0600

    Initial commit

---
+++ app/src/main/java/com/uvg/sign_on/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.uvg.sign_on
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//gradle signingReport
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

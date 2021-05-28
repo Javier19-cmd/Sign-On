d880f1c9806dd484e3325b069d3952dbd59233bb
Javier19-cmd <63107636+Javier19-cmd@users.noreply.github.com>
2021-05-28 14:56:05 -0600

    Anotando el correo en donde se vinculó el firebase de la cuenta.

--- app/src/main/java/com/uvg/sign_on/MainActivity.kt
+++ app/src/main/java/com/uvg/sign_on/MainActivity.kt
@@ -3,6 +3,7 @@ package com.uvg.sign_on
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //gradle signingReport para hacer lo de signingReport.
+//El Firebase de esta app está en la cuenta de sebasvalle@gmail.com.
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
